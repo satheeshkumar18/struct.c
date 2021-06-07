daeb9e2719ef9d4a5bea292760eb78edf9cbd3a4
satheeshkumar18 <70209256+satheeshkumar18@users.noreply.github.com>
2021-06-07 03:44:04 -0700

    Update struct.c

--- struct.c
+++ struct.c
@@ -7,6 +7,7 @@ int rollnumber[2],i;
 float mark[2];
 for(i=0;i<3;i++)
 {
+  printf("enter the numbers\n");
 printf("enter the name, roll number,and marks of the student %d",i+1);
 scanf("%s %d %f",&name[i],&rollnumber[i],&mark[i]);
 scanf("%c",&dummy);
